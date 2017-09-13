c95a2bfff399b5f3aec5a8efe291949de4190f85
Hemanth <Hemanth@HEMANTH-PC>
2017-09-14 00:20:32 +0530

    Add project files.

---
+++ ReadWordDoc/Page1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="ReadWordDoc.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="WordFileToRead" runat="server" Width="500px" /> 
+        <asp:Button ID="btnUpload" runat="server" Text="Read File" OnClick="btnUpload_Click" /> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ ReadWordDoc/Page1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReadWordDoc
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            WordFileToRead.SaveAs(Server.MapPath("/Repository/" + WordFileToRead.FileName));
+            object filename = Server.MapPath("/Repository/" + WordFileToRead.FileName);
+            Application AC = new Application();
+            Document doc = new Document();
+            object readOnly = false;
+            object isVisible = true;
+            object missing = System.Reflection.Missing.Value;
+            try
+            {
+                doc = AC.Documents.Open(ref filename, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible, ref isVisible, ref missing, ref missing, ref missing);
+                string fileContent = doc.Content.Text;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
